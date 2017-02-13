85fe2d840796b2543ca7a1f984439075bc15043b
studiovoyager <tjensen@usc.edu.au>
2017-02-13 16:44:57 +1000

    Up to chapter 18

--- gulpfile.js
+++ gulpfile.js
@@ -9,92 +9,124 @@ var autoPrefixer = require('gulp-autoprefixer');
 var sourcemaps = require('gulp-sourcemaps');
 var spritesmith = require('gulp.spritesmith');
 var gulpIf = require('gulp-if');
+var nunjucksRender = require('gulp-nunjucks-render');
+var data = require('gulp-data');
+var fs = require('fs');
 
 var myGulpOptions = {
-key: 'value',
-key2: 'value2'
+    key: 'value',
+    key2: 'value2'
 };
 
 
 //Functions
-function errorHandler(err){
-	console.log(err.toString());
-	this.emit("end");
+function errorHandler(err) {
+    console.log(err.toString());
+    this.emit("end");
 }
+
 function customPlumber(errTitle) {
-	return plumber({
-		errorHandler: notify.onError({
-			//Customizing error title
-			title: errTitle || "Error running Gulp",
-			message: "Error: <%= error.message %>",
-			//sound: "Glass"
-		})
-	});
+    return plumber({
+        errorHandler: notify.onError({
+            //Customizing error title
+            title: errTitle || "Error running Gulp",
+            message: "Error: <%= error.message %>",
+            //sound: "Glass"
+        })
+    });
 }
 
 /* Tasks */
 
 //Hello Task
-gulp.task('hello', function(){
-	console.log('Hello Tim');
+gulp.task('hello', function() {
+    console.log('Hello Tim');
 });
 
 //Sprites Task
-gulp.task('sprites',function(){
-	gulp.src('app/images/sprites/**/*')
-	.pipe(spritesmith({
-		cssName: '_sprites.scss',
-		imgName: 'sprites.jpg',
-		imgPath: '../images/sprites.jpg',
-		retinaSrcFilter: 'app/images/sprites/*@2x.jpg',
-		retinaImgName: 'sprites@2x.jpg',
-		retinaImgPath: '../images/sprites@2x.jpg'
-	}))
-	.pipe(gulpIf('*.jpg',gulp.dest('app/images')))
-	.pipe(gulpIf('*.scss',gulp.dest('app/scss')))
+gulp.task('sprites', function() {
+    gulp.src('app/images/sprites/**/*')
+        .pipe(spritesmith({
+            cssName: '_sprites.scss',
+            imgName: 'sprites.jpg',
+            imgPath: '../images/sprites.jpg',
+            retinaSrcFilter: 'app/images/sprites/*@2x.jpg',
+            retinaImgName: 'sprites@2x.jpg',
+            retinaImgPath: '../images/sprites@2x.jpg'
+        }))
+        .pipe(gulpIf('*.jpg', gulp.dest('app/images')))
+        .pipe(gulpIf('*.scss', gulp.dest('app/scss')))
 });
 
 //Sass Task
-gulp.task('sass', function(){
-	return gulp.src('app/scss/**/*.scss')
-		//Replace plumber with customPlumber
-		.pipe(customPlumber('Error running Sass'))
-		//Init sourcemaps
-		.pipe(sourcemaps.init())
-		.pipe(sass({
-			includePaths: [
-			'app/bower_components',
-			'node_modules' //if installing sass libraries via npm
-			]
-			}))
-		// ... other plugins
-		.pipe(autoPrefixer({
-			browsers: ['last 2 versions']
-		}))
-		.pipe(sourcemaps.write())
-		.pipe(gulp.dest('app/css'))
-		//Tells Browser Sync to reload files when task done
-		.pipe(browserSync.reload({
-			stream: true
-		}))
+gulp.task('sass', function() {
+    return gulp.src('app/scss/**/*.scss')
+        //Replace plumber with customPlumber
+        .pipe(customPlumber('Error running Sass'))
+        //Init sourcemaps
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            includePaths: [
+                'app/bower_components',
+                'node_modules' //if installing sass libraries via npm
+            ]
+        }))
+        // ... other plugins
+        .pipe(autoPrefixer({
+            browsers: ['last 2 versions']
+        }))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('app/css'))
+        //Tells Browser Sync to reload files when task done
+        .pipe(browserSync.reload({
+            stream: true
+        }))
 });
 
 //Watch Task
-gulp.task('watch', ['browserSync','sass'], function(){
-	gulp.watch('app/scss/**/*.scss',['sass']);
-	gulp.watch('app/js/**/*.js',browserSync.reload);
-	gulp.watch('app/*.html',browserSync.reload);
-	//Other Watchers
+gulp.task('watch', ['browserSync', 'sass'], function() {
+    gulp.watch('app/scss/**/*.scss', ['sass']);
+    gulp.watch('app/js/**/*.js', browserSync.reload);
+    gulp.watch('app/*.html', browserSync.reload);
+    gulp.watch([
+            'app/templates/**/*',
+            'app/pages/**/*.+(html|nunjucks)',
+            'app/data.json'
+            ], ['nunjucks'] //runs Nunjucks task
+            )
+        //Other Watchers
 });
 
 //Browser Sync Task
-gulp.task('browserSync',function(){
-	browserSync({
-		server: {
-			baseDir: 'app'
-		},
-		//proxy: 'www.localhost:8080',
-		browser: ['google chrome']
-	})
+gulp.task('browserSync', function() {
+    browserSync({
+        server: {
+            baseDir: 'app'
+        },
+        //proxy: 'www.localhost:8080',
+        browser: ['google chrome']
+    })
 });
 
+
+gulp.task('nunjucks', function() {
+
+    //nunjucksRender.nunjucks.configure(['app/templates']);
+
+    //Gets .html and .nunjucks files in pages
+    return gulp.src('app/pages/**/*.+(html|nunjucks)')
+   		.pipe(customPlumber('Error running Nunjucks'))
+        //Add data to Nunjucks
+        .pipe(data(function() {
+            return JSON.parse(fs.readFileSync('./app/data.json'))
+        }))
+        //Renders nunjuck files
+        .pipe(nunjucksRender({
+            path: ['app/templates']
+        }))
+        //Output files in app folder
+        .pipe(gulp.dest('app'))
+        .pipe(browserSync.reload({
+        	stream: true
+        }))
+});
